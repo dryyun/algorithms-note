06cd167369952cc6633c507dadd1c9347fd69abd
dryyun <dryyun@gmail.com>
2018-09-05 22:03:46 +0800

    lc 102

---
+++ LeetCode/102.Binary Tree Level Order Traversal/README.md
@@ -0,0 +1,27 @@
+# 二叉树的层次遍历
+
+## 链接
+https://leetcode-cn.com/problems/binary-tree-level-order-traversal/description/
+
+## 描述
+
+给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。  
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+```text
+    3
+   / \
+  9  20
+    /  \
+   15   7
+```
+   
+返回其层次遍历结果：  
+```text
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+```
---
+++ LeetCode/102.Binary Tree Level Order Traversal/binary-tree-level-order-traversal.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+const {LeetCode} = require('../../leetcode.js');
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+    if (!root) {
+        return [];
+    }
+    let stack = [];
+    stack.push([1, root]); // level = 1
+
+    let arr = [];
+    let level;
+    let node;
+    while (stack.length) {
+        [level, node] = stack.shift();
+        if (!node) {
+            continue;
+        }
+
+        stack.push([level + 1, node.left]);
+
+        stack.push([level + 1, node.right]);
+        if (!arr[level]) {
+            arr[level] = [];
+        }
+        arr[level].push(node.val);
+    }
+    arr.shift(); //TODO 使用 arr.slice(1) 就会慢很多的感觉
+    return arr;
+};
+
+let nums = [3, 9, 20, null, null, 15, 7];
+console.log(levelOrder(LeetCode.arrToBinaryTree(nums)));
