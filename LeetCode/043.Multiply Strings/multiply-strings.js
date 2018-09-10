9a396a6ce2dc8b3abb8f2a3ff5aa567022ec867d
dryyun <dryyun@gmail.com>
2018-09-10 20:37:55 +0800

    lc 043

--- LeetCode/043.Multiply Strings/multiply-strings.js
+++ LeetCode/043.Multiply Strings/multiply-strings.js
@@ -4,8 +4,31 @@
  * @return {string}
  */
 var multiply = function (num1, num2) {
-    
+    if (num1 === '0' || num2 === '0') {
+        return '0';
+    }
+
+    let len1 = num1.length;
+    let len2 = num2.length;
+
+    let arr = new Array(len1 + len2).fill(0);
+    for (let i = 0; i < len1; i++) {
+        for (let j = 0; j < len2; j++) {
+            arr[i + j + 1] += Number(num1[i]) * Number(num2[j]);
+        }
+    }
+
+    for (let i = arr.length - 1; i > 0; i--) {
+        arr[i - 1] += Math.floor(arr[i] / 10);
+        arr[i] = arr[i] % 10;
+    }
+
+    if (arr[0] === 0) {
+        arr.shift();
+    }
+    return arr.join('');
+
 };
 
-console.log(multiply('123', '45'));
-
+console.log(multiply('2', '3'));
+console.log(multiply('123', '456'));
