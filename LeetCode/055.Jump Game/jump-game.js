2b2c8543657b2ed3dbe455d1fb8463eb400438be
dryyun <dryyun@gmail.com>
2018-09-11 14:43:31 +0800

    lc 055

--- LeetCode/055.Jump Game/jump-game.js
+++ LeetCode/055.Jump Game/jump-game.js
@@ -3,21 +3,20 @@
  * @return {boolean}
  */
 var canJump = function (nums) {
-    let index = 1;
+    let index = 0;
     let len = nums.length;
     for (let i = 0; i < len; i++) {
-        if (index < i + 1) {
+        if (index < i) {
             break;
         }
-        index = Math.max(index, i + 1 + nums[i]);
-
+        index = Math.max(index, i + nums[i]);
     }
-    return index >= len;
+    return index >= len - 1;
 };
 
 console.log(canJump([2, 3, 1, 1, 4])); // true
 console.log(canJump([3, 2, 1, 0, 4])); // false
 console.log(canJump([2, 0])); // true
 console.log(canJump([2, 5, 0, 0])); // true
-console.log(canJump([0, 1])); // true
+console.log(canJump([0, 1])); // false
 
