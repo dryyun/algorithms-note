e0769bd123de8689bd863993c5c1245f3ac986c4
dryyun <dryyun@gmail.com>
2018-09-13 18:08:22 +0800

    lc 091

---
+++ LeetCode/091.Decode Ways/decode-ways.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function (s) {
+    let len = s.length;
+    if (0 === len || 1 === len) {
+        return len;
+    }
+
+    // let arr = [];
+    // for (let i = 1; i <= 26; i++) {
+    //     arr[i] = String.fromCharCode(64 + i);
+    // }
+    if (2 === len) {
+        if (Number(s) > 26) {
+            return 2;
+        }
+        return 3;
+    }
+
+    
+    let dfs = function () {
+
+    };
+    dfs();
+
+
+};
+
+// console.log(numDecodings('2'));
+// console.log(numDecodings('12'));
+// console.log(numDecodings('27'));
+
+console.log(numDecodings('226'));
