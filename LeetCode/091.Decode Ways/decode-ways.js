6c8b54ea654126a15dc1fde9a5788602cf379c9f
dryyun <dryyun@gmail.com>
2018-09-14 14:45:26 +0800

    lc 091

--- LeetCode/091.Decode Ways/decode-ways.js
+++ LeetCode/091.Decode Ways/decode-ways.js
@@ -4,32 +4,40 @@
  */
 var numDecodings = function (s) {
     let len = s.length;
-    if (0 === len || 1 === len) {
-        return len;
+    let ns = Number(s);
+    if (0 === ns || s.substr(0, 1) === '0' || 0 === len) {
+        return 0;
     }
 
-    // let arr = [];
-    // for (let i = 1; i <= 26; i++) {
-    //     arr[i] = String.fromCharCode(64 + i);
-    // }
-    if (2 === len) {
-        if (Number(s) > 26) {
-            return 2;
+    let prev = 0;
+    let cur = 1;
+    for (let i = 1; i <= len; i++) {
+        if (s[i - 1] === '0') {
+            cur = 0;
         }
-        return 3;
+        if (i < 2 || !(s[i - 2] === '1' || (s[i - 2] === '2' && s[i - 1] <= '6'))) {
+            prev = 0;
+        }
+        [prev, cur] = [cur, prev + cur];
     }
-
-
-    let dfs = function () {
-
-    };
-    dfs();
-
-
+    return cur;
 };
 
-// console.log(numDecodings('2'));
-// console.log(numDecodings('12'));
-// console.log(numDecodings('27'));
+// console.log(numDecodings('2')); // 1:   2
+// console.log(numDecodings('12')); // 2:  1,2 || 12
+// console.log(numDecodings('27')); // 1:  2,7
+// console.log(numDecodings('226')); // 3: 2,2,6 || 22,6 || 2,26
+// console.log(numDecodings('227')); // 2: 2,2,7 || 22,7
+// console.log(numDecodings('333')); // 1: 3,3,3
+// console.log(numDecodings('301'));//  1: 3,01    3,01 ||3 0 1
+// console.log(numDecodings('0'));// 0:
+// console.log(numDecodings('10'));// 1:
+// console.log(numDecodings('00'));// 0
+// console.log(numDecodings('01'));// 0
+//
+// console.log(numDecodings('012'));// 0
+// console.log(numDecodings('272'));// 0
+// console.log(numDecodings('230'));// 0
+console.log(numDecodings('12120'));// 12120 // 3：    1，2，1，20 || 12，1，20 || 1，21，20
 
-console.log(numDecodings('226'));
+
