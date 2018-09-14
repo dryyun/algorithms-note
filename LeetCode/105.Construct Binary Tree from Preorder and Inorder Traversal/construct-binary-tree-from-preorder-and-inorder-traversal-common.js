28db089e2628f8a769443173c370806f2f8022c5
dryyun <dryyun@gmail.com>
2018-09-14 21:33:28 +0800

    lc 105

---
+++ LeetCode/105.Construct Binary Tree from Preorder and Inorder Traversal/construct-binary-tree-from-preorder-and-inorder-traversal-common.js
@@ -0,0 +1,38 @@
+/**
+ * 常规思路，就是一直 array.slice 影响执行效率。
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+const {LeetCode, TreeNode} = require('../../leetcode.js');
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+    if (preorder.length === 0) {
+        return null;
+    }
+    let root = preorder[0];
+    let rootIndex = inorder.indexOf(root);
+
+    let leftInOrder = inorder.slice(0, rootIndex);
+    let rightInOrder = inorder.slice(rootIndex + 1);
+    let leftInOrderLen = leftInOrder.length;
+    // let rightInOrderLen = rightInOrder.length;
+    let leftPreOrder = preorder.slice(1, leftInOrderLen + 1);
+    let rightPreOrder = preorder.slice(leftInOrderLen + 1);
+
+    let rnode = new TreeNode(root);
+
+    rnode.left = buildTree(leftPreOrder, leftInOrder);
+    rnode.right = buildTree(rightPreOrder, rightInOrder);
+    return rnode;
+};
+
+console.log(LeetCode.binaryTreeToArr(buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7])));
--- LeetCode/105.Construct Binary Tree from Preorder and Inorder Traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ LeetCode/105.Construct Binary Tree from Preorder and Inorder Traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -12,24 +12,23 @@ const {LeetCode, TreeNode} = require('../../leetcode.js');
  * @return {TreeNode}
  */
 var buildTree = function (preorder, inorder) {
-    if (preorder.length === 0) {
-        return null;
-    }
-    let root = preorder[0];
-    let rootIndex = inorder.indexOf(root);
 
-    let leftInOrder = inorder.slice(0, rootIndex);
-    let rightInOrder = inorder.slice(rootIndex + 1);
-    let leftInOrderLen = leftInOrder.length;
-    // let rightInOrderLen = rightInOrder.length;
-    let leftPreOrder = preorder.slice(1, leftInOrderLen + 1);
-    let rightPreOrder = preorder.slice(leftInOrderLen + 1);
+    let bt = function (preorder, pres, preend, inorder, ins, inend) {
+        if (pres > preend) {
+            return null;
+        }
 
-    let rnode = new TreeNode(root);
+        let root = preorder[pres];
+        let rootIndex = inorder.indexOf(root);
+        let leftInOrderLen = rootIndex - ins;
 
-    rnode.left = buildTree(leftPreOrder, leftInOrder);
-    rnode.right = buildTree(rightPreOrder, rightInOrder);
-    return rnode;
+        let rnode = new TreeNode(root);
+        rnode.left = bt(preorder, pres + 1, pres + leftInOrderLen, inorder, ins, ins + leftInOrderLen - 1);
+        rnode.right = bt(preorder, pres + leftInOrderLen + 1, preend, inorder, ins + leftInOrderLen + 1, inend);
+        return rnode;
+    };
+
+    return bt(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
 };
 
 console.log(LeetCode.binaryTreeToArr(buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7])));
