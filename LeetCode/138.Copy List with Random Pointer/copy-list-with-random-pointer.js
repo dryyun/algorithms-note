796f4dd88242102add5a123beb9afce7d6164060
dryyun <dryyun@gmail.com>
2018-09-18 17:52:43 +0800

    lc 138

---
+++ LeetCode/138.Copy List with Random Pointer/copy-list-with-random-pointer.js
@@ -0,0 +1,43 @@
+// Definition for singly-linked list with a random pointer.
+function RandomListNode(label) {
+    this.label = label;
+    this.next = this.random = null;
+}
+
+
+/**
+ * @param {RandomListNode} head
+ * @return {RandomListNode}
+ */
+var copyRandomList = function (head) {
+    if (!head)
+        return null;
+
+    let hash = new Map();
+    let newArr = [];
+    let node = head;
+
+    while (node) {
+        hash.set(node, newArr.length);
+        newArr.push(new RandomListNode(node.label));
+        node = node.next;
+    }
+
+    node = head;
+    for (let i = 0, len = newArr.length; i < len; i++) {
+        if (i !== len - 1) {
+            newArr[i].next = newArr[i + 1];
+        }
+
+        let random = node.random;
+        let index = hash.get(random);
+        if (index !== undefined) {
+            newArr[i].random = newArr[index];
+        }
+
+        node = node.next;
+    }
+
+    return newArr[0];
+};
+
