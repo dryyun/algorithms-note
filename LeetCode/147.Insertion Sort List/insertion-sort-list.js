2f3c8fd5806f21c8670478ac67f49bfb206fed45
dryyun <dryyun@gmail.com>
2018-09-19 11:52:57 +0800

    lc 147

---
+++ LeetCode/147.Insertion Sort List/insertion-sort-list.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+const {LeetCode, ListNode} = require('../../leetcode.js');
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function (head) {
+    if (!head) {
+        return head;
+    }
+    let nodes = [];
+    while (head) {
+        nodes.push(head);
+        head = head.next;
+    }
+    nodes.sort((a, b) => a.val - b.val);
+    let first = node = nodes[0];
+    for (let i = 1, len = nodes.length; i < len; i++) {
+        node.next = nodes[i];
+        nodes[i].next = null;
+        node = nodes[i];
+    }
+    return first;
+};
+
+let list = LeetCode.arrToList([4, 2, 1, 3]);
+LeetCode.showList(insertionSortList(list));
+
+list = LeetCode.arrToList([-1, 5, 3, 4, 0]);
+LeetCode.showList(insertionSortList(list));
